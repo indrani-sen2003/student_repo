03e259fdcffb9d6ec5f5880289c28be633dd91b0
indrani-sen2003 <51267692+indrani-sen2003@users.noreply.github.com>
2020-11-26 22:17:42 +0530

    added the field majored to model student

--- studapp/models.py
+++ studapp/models.py
@@ -4,5 +4,7 @@ class stud(models.Model):
 	roll=models.IntegerField(primary_key=True)
 	sname=models.CharField(max_length=30)
 	dob=models.DateField()
+    majored=models.CharField(max_length=30)
+
     
 # Create your models here.
