3c8c388acb300a661d71b64fa37f3ba87af44cad
indrani-sen2003 <51267692+indrani-sen2003@users.noreply.github.com>
2020-11-26 22:10:40 +0530

    added model course

--- studapp/models.py
+++ studapp/models.py
@@ -4,5 +4,9 @@ class stud(models.Model):
 	roll=models.IntegerField(primary_key=True)
 	sname=models.CharField(max_length=30)
 	dob=models.DateField()
-    
+
+class course(models.Model):
+    course_code=models.IntegerField(primary_key=True)
+    course_name=models.CharField(max_length=30)
+
 # Create your models here.
